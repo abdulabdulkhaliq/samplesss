44df2588996a91d736b6ddd76abe4e94f4bd7e19
abdulabdulkhaliq <86907166+abdulabdulkhaliq@users.noreply.github.com>
2021-12-23 18:48:51 +0500

    basic module

---
+++ sales_customization_tnoc/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Customization Telenoc',
+    'category': '',
+    'summary': 'For the customization in the sales module',
+    'description': """
+        we have done customization in the sales modules. 
+""",
+    'license': 'LGPL Version 3',
+    'depends': ['sale'],
+    'data': [
+        'views/customization_sales.xml',
+    ],
+    'author': 'AbdulKhaliq',
+
+    }
+
